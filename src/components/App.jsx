8acb7402ad5357820967f18b4ff9ab0e0f981ab5
Andrii-Semeniv <andrii.semeniv7@gmail.com>
2023-04-10 23:57:12 +0300

    startRefactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import Statistics from './statistics/Statistics';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  addGoodPoint = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+
+  addNeutralPoint = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+
+  addBadPoint = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total > 0 ? Math.round((good / total) * 100) : 0;
+  };
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedbacks();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <div>
+          <button onClick={this.addGoodPoint}>Good</button>
+          <button onClick={this.addNeutralPoint}>Neutral</button>
+          <button onClick={this.addBadPoint}>Bad</button>
+        </div>
+        <p>Statistics</p>
+        <div>
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
